7a93c985c6e4b4b38bea8f368b5fe7757d3cf597
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-17 21:25:02 +0000

    [SO] update header to be apache

--- trunk/blueprint/extender/src/main/scala/beans/package.scala
+++ trunk/blueprint/extender/src/main/scala/beans/package.scala
@@ -1,9 +1,9 @@
 /**
  * Copyright (c) 2010 DIKKA.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Apache License, v. 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.apache.org/licenses/
  *
  * Contributors:
  *   Slim Ouertani   - initial API and implementation
@@ -13,13 +13,8 @@ package net.dikka.charika.blueprint.extender.impl
 
 
 package object beans {
-
- // abstract class Component (id:String, activation :Activation=Eager, dependsOn:List[String])
-
-
-
-
-  def xor3(x: Boolean, y: Boolean, z : Boolean): Boolean = (x || y || z ) && !(x && y) && !(x || z) && !(y || z) && ! (x && y && z)
+  
+  def xor(x: Boolean, y: Boolean, z : Boolean): Boolean = (x || y || z ) && !(x && y) && !(x || z) && !(y || z) && ! (x && y && z)
 
 
   def xor(x: Boolean, y: Boolean): Boolean = (x || y) && !(x && y)
--- trunk/blueprint/extender/src/main/scala/parser/package.scala
+++ trunk/blueprint/extender/src/main/scala/parser/package.scala
@@ -1,15 +1,14 @@
 /**
  * Copyright (c) 2010 DIKKA.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Apache License, v. 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.apache.org/licenses/
  *
  * Contributors:
  *   Slim Ouertani   - initial API and implementation
  *
  */
-
 package net.dikka.charika.blueprint.extender.impl
 
 
