f1b8dfe8d3063062394ba7f45bdc38fdf37854c4
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-18 09:30:23 +0000

    [SO] bean with compat form, test are updated

--- trunk/blueprint/extender/src/test/scala/BeanSpec.scala
+++ trunk/blueprint/extender/src/test/scala/BeanSpec.scala
@@ -18,122 +18,77 @@ import org.specs.mock.Mockito
 
 
 class BeanSpec extends SpecificationWithJUnit with Mockito {
-  
-  
-  
 
   """ The destroyMethod mustNot be set when the scope is prototype."""  in {
-    Bean(id="id",
-         activation=Lazy,
-         scope=Prototype,
-         dependsOn=List(),
-         arguments=List(),
-         propertys=List(),
-         clazz=Some("class"),
-         factoryMethod=Some(" initMethod:String,"),
-         factoryRef=Some(" destroyMethod"),
-         initMethod =None,
-         destroyMethod =Some(" destroyMethod")
-    ) must throwA[IllegalArgumentException]
+     Bean("id", Lazy, Prototype,
+             ConstructionParam(List(),List(),List()),
+             Construction( Some("class"), Some(" initMethod:String,"), Some(" destroyMethod")),
+             Callback (None,Some(" factoryRef:String,"))
+    )   must throwA[IllegalArgumentException]
   }
 
+
+
+
+
+  """ The activation must not be set to eager if the bean also has prototype scope.""" in {
+     Bean("id",
+             Eager,
+             Prototype,
+             ConstructionParam(List(),List(),List()),
+             Construction(Some("class"),  None, None),
+             Callback(None, None)
+    ) must throwA[IllegalArgumentException]
+
+  }
+
+  """The following combinations of arguments are valid, all other combinations are invalid:
+• className
+• className, factory-method
+• factory-ref, factory-method
+""" in {
+    Bean ("id",  Lazy, Singleton,
+          ConstructionParam(List(),List(),List()),
+          Construction(Some("class"), None,None),
+          Callback( Some("init"), Some("destroy"))
+    )
+
+
+    Bean ("id",
+          Lazy,
+          Singleton,
+          ConstructionParam(List(),List(),List()),
+          Construction(Some("class"), Some("factory-method"),None),
+          Callback(None, None)
+    )
+
+
+    Bean ("id",
+          Lazy,
+          Singleton,
+          ConstructionParam(List(),List(),List()),
+          Construction(None, Some("factory-method"),Some("factory-ref")),
+          Callback(None, None)
+    )
+
+
+    Bean ("id",
+          Lazy,
+          Singleton,
+          ConstructionParam(List(),List(),List()),
+          Construction(None, None,None),
+          Callback(None, None)
+    )must throwA[IllegalArgumentException]
+
+    Bean ("id",
+          Lazy,
+          Singleton,
+          ConstructionParam(List(),List(),List()),
+          Construction(Some(""), Some(""),Some("")),
+          Callback(None, None)
+    )must throwA[IllegalArgumentException]
+  }
 }
 
-//  """ The destroyMethod mustNot be set when the scope is prototype.""" in {
-//
-//    Bean("id",
-//         Lazy,
-//         Prototype,
-//         List(),
-//         List(),
-//         List(),
-//         Some("class"),
-//         Some(" initMethod:String,"),
-//         Some(" destroyMethod"),
-//         None,
-//         None
-//    ) must throwA[IllegalArgumentException]
-
-//    new Bean("id",
-//             Lazy,
-//             Prototype,
-//             ConstructionParam(List(),List(),List()),
-//             Construction( Some("class"), Some(" initMethod:String,"), Some(" destroyMethod")),
-//             Callback (None,Some(" factoryRef:String,"))
-//    )   must throwA[IllegalArgumentException]
-//  }
-//
-//  """ The activation must not be set to eager if the bean also has prototype scope.""" in {
-//    new Bean("id",
-//             Eager,
-//             Prototype,
-//             List(),
-//             List() ,
-//             List(),
-//             Some("class"),
-//             None,
-//             None,
-//             None,
-//             None
-//    ) must throwA[IllegalArgumentException]
-//  }
-//
-//  """The following combinations of arguments are valid, all other combinations are invalid:
-//• className
-//• className, factory-method
-//• factory-ref, factory-method
-//""" in {
-//
-//
-//    new Bean ("id",
-//              Lazy,
-//              Singleton,
-//              ConstructionParam(),
-//              Construction(Some("class"), None,None),
-//              Callback( Some("init"), Some("destroy"))
-//    )
-//
-//    new Bean ("id",
-//              Lazy,
-//              Singleton,
-//              ConstructionParam(),
-//              Construction(Some("class"), Some("factory-method"),None),
-//              Callback(None, None)
-//    )
-//
-//    new Bean ("id",
-//              Lazy,
-//              Singleton,
-//              ConstructionParam(),
-//              Construction(None, Some("factory-method"),Some("factory-ref")),
-//              Callback(None, None)
-//    )
-//
-//
-//
-//    new Bean ("id",
-//              Lazy,
-//              Singleton,
-//              ConstructionParam(),
-//              Construction(None, None,None),
-//              Callback(None, None)
-//    )must throwA[IllegalArgumentException]
-//
-//
-//
-//    new Bean ("id",
-//              Lazy,
-//              Singleton,
-//              ConstructionParam(),
-//              Construction(Some(""), Some(""),Some("")),
-//              Callback(None, None)
-//    )must throwA[IllegalArgumentException]
-//
-//
-//
-//  }
-
-
-//}
  
   
