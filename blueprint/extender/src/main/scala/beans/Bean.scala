d4078ed90b920c109e3d2a69befca8745ed702b6
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-18 11:21:07 +0000

    [SO] update xor3

--- blueprint/extender/src/main/scala/beans/Bean.scala
+++ blueprint/extender/src/main/scala/beans/Bean.scala
@@ -12,35 +12,6 @@
 package net.dikka.charika.blueprint.extender.impl
 package beans
 
-
-case class Callback(initMethod:Option[String], destroyMethod:Option[String]){
-  require(initMethod != null, "InitMethod must not be null!")
-  require(destroyMethod != null, "DestroyMethod must not be null!")
-}
-
-case class Construction (clazz:Option[String], factoryMethod: Option[String], factoryRef:Option[String]){
-  require(clazz != null, "Class must not be null!")
-  require(factoryMethod != null, "FactoryMethod must not be null!")
-  require(factoryRef != null, "FactoryRef must not be null!")
-
-
-    if(clazz isDefined) {
-    require((factoryRef  isEmpty), "Invalid combination" )
-  }
-
-  if(factoryMethod isDefined) {
-    require(xor( factoryRef isDefined ,  clazz isDefined ) , "Invalid combination" )
-  }
-  require ((clazz isDefined ) || (factoryMethod isDefined) || (factoryRef isDefined ),"Invalid combination" )
-}
-
-case class ConstructionParam( dependsOn:List[String], arguments :List[Argument],  propertys : List[Property]) {
-  require(dependsOn != null, "DependsOn must not be null!")
-  require(arguments != null, "arguments must not be null!")
-  require(propertys != null, "propertys must not be null!")
-}
-
-
 case class Bean (id:String,
                  activation:Activation,
                  scope:Scope ,
@@ -48,35 +19,12 @@ case class Bean (id:String,
                  construction :Construction,
                  callback :Callback) extends Component(id, activation, constructionParam.dependsOn) {
 
- 
-
   require(id != null, "Id must not be null!")
   require(activation != null, "Activation must not be null!")
   require(scope != null, "Scope must not be null!")
-
- 
-
-
-
-  /*
-   * The destroyMethod must not be set when the scope is prototype.
-   The activation must not be set to eager if the bean also has prototype scope.
-   The following combinations of arguments are valid, all other combinations are invalid:
-   • className
-   • className, factory-method
-   • factory-ref, factory-method
-   */
 
   if(scope == Prototype) {
     require (callback.destroyMethod isEmpty,"The destroyMethod must not be set when the scope is prototype.")
     require (activation == Lazy, "The activation must not be set to eager if the bean also has prototype scope.")
   }
-
-
-
-
-
-
-
-
 }
