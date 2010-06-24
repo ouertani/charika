0dc8a4ba88237418fd6cfabce06f89d20a3bb713
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-24 11:26:29 +0000

    [SO] intermidiate correction

---
+++ blueprint/extender/src/main/scala/reflect/impl/package.scala
@@ -0,0 +1,35 @@
+package net.dikka.charika.blueprint
+package reflect
+
+import org.osgi.service.blueprint.container.ComponentDefinitionException
+import scala.xml.Node
+
+class impl {
+  def need(b :Boolean, msg : String) {
+    if(!b) throw new ComponentDefinitionException("Unknown element " + msg);
+  }
+
+  trait ComponentParser [T] {
+    def  parseElement( node:Node):T
+  }
+
+
+
+  implicit def toScope(scope: String): Scope={
+    if(scope== null) Singleton
+    if(scope=="singleton")  Singleton
+    else if(scope =="prototype") Prototype
+    else if (scope isEmpty) Singleton
+    else throw new IllegalArgumentException("invalid scope text")
+  }
+
+
+
+  implicit def toActivation(activation: String): Activation = {
+    if(activation ==null ) Eager
+    if(activation=="lazy") Lazy
+    else if(activation=="eager") Eager
+    else if(activation isEmpty) Eager
+    else throw new IllegalArgumentException("invalid activation text")
+  }
+}
