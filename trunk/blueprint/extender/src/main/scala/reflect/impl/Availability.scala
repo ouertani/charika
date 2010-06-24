50ae9cc690d53366817e927824b23952a3af81f3
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-24 10:49:26 +0000

    [SO] intermidiate correction

---
+++ trunk/blueprint/extender/src/main/scala/reflect/impl/Availability.scala
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2010 DIKKA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v. 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/
+ *
+ * Contributors:
+ *   Slim Ouertani   - initial API and implementation
+ *
+ */
+package net.dikka.charika.blueprint
+package reflect.impl
+
+sealed trait Availability{
+  val intValue=1
+}
+case object Mandatory extends Availability
+case object Optional extends Availability{
+   override val intValue=2
+}
---
+++ trunk/blueprint/extender/src/main/scala/reflect/impl/MemberType.scala
@@ -0,0 +1,14 @@
+package net.dikka.charika.blueprint
+package reflect.impl
+
+sealed trait MemberType
+{
+  val intValue: Int
+}
+case object ObjectMemberType extends MemberType{
+  override val intValue =1
+}
+case object ReferenceMemberType extends MemberType{
+  override val intValue =2
+}
+
