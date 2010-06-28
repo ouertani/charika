c995bc953f042f4eef52626d1d7f5c32104f5d15
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-28 12:55:09 +0000

    [SO] adding draft build and validate

---
+++ trunk/blueprint/extender/src/main/scala/reflect/TBuilder.scala
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2010 DIKKA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v. 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/
+ *
+ * Contributors:
+ *   Slim Ouertani   - initial API and implementation
+ *
+ */
+package net.dikka.charika.blueprint
+package reflect
+
+import org.osgi.service.blueprint.container.ComponentDefinitionException
+
+trait TBuilder[T] extends Function0[T] {
+ 
+
+  override def apply ():T={
+    validate
+    this.apply
+
+  }
+  @throws(classOf[ComponentDefinitionException])
+  def validate():Unit
+}
