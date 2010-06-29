e7e925b800671eb171158dc77a1fcf523579a51f
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-29 17:59:23 +0000

    [SO] moving vals

--- blueprint/extender/src/main/scala/parser/package.scala
+++ blueprint/extender/src/main/scala/parser/package.scala
@@ -19,49 +19,79 @@ import org.osgi.service.blueprint.container._
 
 package object parser{
 
+  val BLUEPRINT_NAMESPACE = "http://www.osgi.org/xmlns/blueprint/v1.0.0";
+  val  BLUEPRINT_ELEMENT = "blueprint";
+  val  DESCRIPTION_ELEMENT = "description";
+  val  TYPE_CONVERTERS_ELEMENT = "type-converters";
+  val  BEAN_ELEMENT = "bean";
+  val  ARGUMENT_ELEMENT = "argument";
+  val  REF_ELEMENT = "ref";
+  val  IDREF_ELEMENT = "idref";
+  val  LIST_ELEMENT = "list";
+  val  SET_ELEMENT = "set";
+  val  MAP_ELEMENT = "map";
+  val  ARRAY_ELEMENT = "array";
+  val  PROPS_ELEMENT = "props";
+  val  PROP_ELEMENT = "prop";
+  val  PROPERTY_ELEMENT = "property";
+  val  NULL_ELEMENT = "null";
+  val  VALUE_ELEMENT = "value";
+  val  SERVICE_ELEMENT = "service";
+  val  REFERENCE_ELEMENT = "reference";
+  val  REFERENCE_LIST_ELEMENT = "reference-list";
+  val  INTERFACES_ELEMENT = "interfaces";
+  val  REFERENCE_LISTENER_ELEMENT = "reference-listener";
+  val  SERVICE_PROPERTIES_ELEMENT = "service-properties";
+  val  REGISTRATION_LISTENER_ELEMENT = "registration-listener";
+  val  ENTRY_ELEMENT = "entry";
+  val  KEY_ELEMENT = "key";
+  val  DEFAULT_ACTIVATION_ATTRIBUTE = "default-activation";
+  val  DEFAULT_TIMEOUT_ATTRIBUTE = "default-timeout";
+  val  DEFAULT_AVAILABILITY_ATTRIBUTE = "default-availability";
+  val  NAME_ATTRIBUTE = "name";
+  val  ID_ATTRIBUTE = "id";
+  val  CLASS_ATTRIBUTE = "class";
+  val  INDEX_ATTRIBUTE = "index";
+  val  TYPE_ATTRIBUTE = "type";
+  val  VALUE_ATTRIBUTE = "value";
+  val  VALUE_REF_ATTRIBUTE = "value-ref";
+  val  KEY_ATTRIBUTE = "key";
+  val  KEY_REF_ATTRIBUTE = "key-ref";
+  val  REF_ATTRIBUTE = "ref";
+  val  COMPONENT_ID_ATTRIBUTE = "component-id";
+  val  INTERFACE_ATTRIBUTE = "interface";
+  val  DEPENDS_ON_ATTRIBUTE = "depends-on";
+  val  AUTO_EXPORT_ATTRIBUTE = "auto-export";
+  val  RANKING_ATTRIBUTE = "ranking";
+  val  TIMEOUT_ATTRIBUTE = "timeout";
+  val  FILTER_ATTRIBUTE = "filter";
+  val  COMPONENT_NAME_ATTRIBUTE = "component-name";
+  val  AVAILABILITY_ATTRIBUTE = "availability";
+  val  REGISTRATION_METHOD_ATTRIBUTE = "registration-method";
+  val  UNREGISTRATION_METHOD_ATTRIBUTE = "unregistration-method";
+  val  BIND_METHOD_ATTRIBUTE = "bind-method";
+  val  UNBIND_METHOD_ATTRIBUTE = "unbind-method";
+  val  KEY_TYPE_ATTRIBUTE = "key-type";
+  val  VALUE_TYPE_ATTRIBUTE = "value-type";
+  val  MEMBER_TYPE_ATTRIBUTE = "member-type";
+  val  SCOPE_ATTRIBUTE = "scope";
+  val  INIT_METHOD_ATTRIBUTE = "init-method";
+  val  DESTROY_METHOD_ATTRIBUTE = "destroy-method";
+  val  ACTIVATION_ATTRIBUTE = "activation";
+  val  FACTORY_REF_ATTRIBUTE = "factory-ref";
+  val  FACTORY_METHOD_ATTRIBUTE = "factory-method";
+  val  AUTO_EXPORT_DISABLED = "disabled";
+  val  AUTO_EXPORT_INTERFACES = "interfaces";
+  val  AUTO_EXPORT_CLASS_HIERARCHY = "class-hierarchy";
+  val  AUTO_EXPORT_ALL = "all-classes";
 
-  implicit def toOptionOfString(string :String):Option[String]= {
-    if(string==null || string.isEmpty) None
-    else Some(string)
-  }
-
-  implicit def toString(op :Option[String])= op match {
-    case Some(x)=>x
-    case None =>null
-  }
-
- 
-
-  implicit def toString(node:scala.xml.NodeSeq):String=node.text
-
-  implicit def toOptionOfInt(string : String):Option[Int]= {
-    try {
-      if(string == null || string.isEmpty ) None
-      else Some( Int.unbox(string))
-    }catch {
-
-      case _ => throw new IllegalArgumentException("Inavalid index found [" + string +"]")
-    }
-  }
+  val  USE_SERVICE_OBJECT = "service-object";
+  val  USE_SERVICE_REFERENCE = "service-reference";
 
 
-
-
-
-  implicit def toOption[T](t:T):Option[T]={if(t!=null) Some(t) else None}
-
-
-
-
-
-  def need(b :Boolean, msg : String):Unit={
-    if(!b) throw new ComponentDefinitionException("Unknown element " + msg);
-   
-  }
   def error( msg : String) : Nothing={
-   throw new ComponentDefinitionException("Unknown element " + msg);
+    throw new ComponentDefinitionException("Unknown element " + msg);
   }
-
 
  
 }
