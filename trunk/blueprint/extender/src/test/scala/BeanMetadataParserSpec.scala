101d2d6e69ef7f194f019fc108b054924208badc
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-30 10:28:53 +0000

    [SO] to start testing

--- trunk/blueprint/extender/src/test/scala/BeanMetadataParserSpec.scala
+++ trunk/blueprint/extender/src/test/scala/BeanMetadataParserSpec.scala
@@ -14,68 +14,42 @@ package parser
 
 import org.specs.SpecificationWithJUnit
 import org.specs.mock.Mockito
-import  net.dikka.charika.blueprint.extender.impl.beans._
+import  net.dikka.charika.blueprint.parser
+import  net.dikka.charika.blueprint.reflect.impl._
+import  net.dikka.charika.blueprint.reflect._
 
 class BeanMetadataParserSpec extends SpecificationWithJUnit with Mockito {
 
 
 
-  """custructor with argument""" in {
+  """simple""" in {
 
-    val xml = <bean id="id" class="clazz">
-      <argument value="arg1" />
+    val xml = <bean id="id" class="clazz">      
               </bean>
 
 
-    val srcBean:Bean = new BeanParser().parseElement(xml)
+    val srcBean =   BeanMetadataParserComponent.beanMetadataParser
 
 
-    val arg = Argument(None,None,None,Some("arg1"),None)
+   
 
 
-    val destBean = Bean("id",
-                        Eager,
-                        Singleton ,
-                        ConstructionParam(List(),List(arg),List()),
-                        Construction(Some("clazz"),None,None),
-                        Callback(None,None))
+    val destBean = new BeanMetadata(
+      "id" ,
+      Eager,
+      List(),
+      Option("clazz"),
+      None ,
+      None,
+      List(),
+      List(),
+      None,
+      null,
+      Singleton)
 
     srcBean mustEqual destBean
   }
 
-  """cosntructor with argument with type""" in {
-
-    val xml=  <bean id="id" class="clazz">
-      <argument type="java.lang.Integer" value="1"/>
-              </bean>
-
-    val srcBean= new BeanParser().parseElement(xml)
-    val arg = new ArgumentBuilder withType "java.lang.Integer" withValue "1" 
-    
-    val destBean= Bean("id",Eager,Singleton, ConstructionParam(List(),List(arg),List()),
-                       Construction(Some("clazz"),None,None),
-                       Callback(None,None))
-    srcBean mustEqual destBean
-  }
-
-
-  """constructor with multiple argument""" in {
-
-    val xml =     <bean id="id" class="clazz"
-      factory-method = "create" >
-      <argument value="arg1"/>
-      <argument value="arg2"/>
-                  </bean>
-
-
-    val srcBean= new BeanParser().parseElement(xml)
-    val arg1 =  (new ArgumentBuilder withValue "arg1" ) +  (    new ArgumentBuilder withValue "arg2"  )
-    val destBean= Bean("id",Eager,Singleton, ConstructionParam(List(),arg1,List()),
-                       Construction(Some("clazz"),None,None),
-                       Callback(None,None))
-
-    srcBean mustEqual destBean
-
-  }
+ 
     
 }
