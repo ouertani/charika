b246714122fdd837ab3734922152be66a44f70c9
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-06-30 14:57:53 +0000

    [SO] second test pass

--- blueprint/extender/src/main/scala/reflect/impl/BeanArgument.scala
+++ blueprint/extender/src/main/scala/reflect/impl/BeanArgument.scala
@@ -14,7 +14,7 @@ package reflect.impl
 
 import reflect._
 
-case class BeanArgument (val value : TMetadata, val valueType : Option[String],val index : Option[Int])
+case class BeanArgument (val value : TMetadata, val valueType : Option[String],val index : Option[Int]=Some(-1))
 extends TBeanArgument {
   override def getValue()=value
   override def getValueType()=valueType
