0321b8d95b6946f99ea4a843f641d7b0ca17fe29
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-07-02 11:12:41 +0000

    [SO] adding defaults in progress

---
+++ trunk/blueprint/extender/src/test/scala/BeanDefaultConfusion.scala
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2010 DIKKA.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License, v. 2.0
+ * which accompanies this distribution, and is available at
+ * http://www.apache.org/licenses/
+ *
+ * Contributors:
+ *   Slim Ouertani   - initial API and implementation
+ *
+ */
+package net.dikka.charika.blueprint
+package parser
+
+import org.specs.SpecificationWithJUnit
+import org.specs.mock.Mockito
+import  net.dikka.charika.blueprint.parser
+
+
+import  net.dikka.charika.blueprint.reflect._
+import  net.dikka.charika.blueprint.reflect.impl._
+import  org.osgi.service.blueprint.container._
+
+class BeanDefaultConfusion extends SpecificationWithJUnit with Mockito {
+
+  val bpp=new BlueprintMetadataParser()
+  """simple""" in {
+
+    val xml = <blueprint xmlns="http://www.osgi.org/xmlns/blueprint/v1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.osgi.org/xmlns/blueprint/v1.0.0"
+      default-activation="lazy" >
+
+      <bean id="id" class="clazz">
+
+      </bean>
+
+              </blueprint >
+
+
+    val destBean = BeanMetadata(
+      "id" ,
+      Lazy,
+      List(),
+      Option("clazz"),
+      None ,
+      None,
+      List(),
+      List(),
+      None,
+      None,
+      Singleton)
+
+    val d= Default(Lazy,
+                   300000,
+                   Mandatory)
+
+    val bpm =BlueprintMetadata(d,List(destBean))
+    val bp =bpp (xml)
+    bp mustNot throwA[Exception]
+
+    bp mustEqual bpm
+
+
+  }
+
+}
