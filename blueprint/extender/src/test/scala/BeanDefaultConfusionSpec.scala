7e723e307b9ce3e4660f640fd800504f89d19354
ouertani <ouertani@f9be30d3-8add-49ba-8348-588e36773ab5>
2010-07-02 11:26:04 +0000

    [SO] add second test

--- blueprint/extender/src/test/scala/BeanDefaultConfusionSpec.scala
+++ blueprint/extender/src/test/scala/BeanDefaultConfusionSpec.scala
@@ -24,14 +24,14 @@ import  org.osgi.service.blueprint.container._
 class BeanDefaultConfusionSpec extends SpecificationWithJUnit with Mockito {
 
   val bpp=new BlueprintMetadataParser()
-  """simple""" in {
+  """override all""" in {
 
     val xml = <blueprint xmlns="http://www.osgi.org/xmlns/blueprint/v1.0.0"
       xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
       xsi:schemaLocation="http://www.osgi.org/xmlns/blueprint/v1.0.0"
       default-activation="lazy" default-timeout="5000" default-availability="optional">
 
-      <bean id="id" class="clazz">
+      <bean id="id" class="clazz" >
 
       </bean>
 
@@ -57,11 +57,52 @@ class BeanDefaultConfusionSpec extends SpecificationWithJUnit with Mockito {
 
     val bpm =BlueprintMetadata(d,List(destBean))
     val bp =bpp (xml)
+    
     bp mustNot throwA[Exception]
-
     bp mustEqual bpm
 
 
   }
 
+
+
+  """bi override all""" in {
+
+    val xml = <blueprint xmlns="http://www.osgi.org/xmlns/blueprint/v1.0.0"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.osgi.org/xmlns/blueprint/v1.0.0"
+      default-activation="lazy" default-timeout="5000" default-availability="optional">
+
+      <bean id="id" class="clazz" activation="eager">
+
+      </bean>
+
+              </blueprint >
+
+
+    val destBean = BeanMetadata(
+      "id" ,
+      Eager,
+      List(),
+      Option("clazz"),
+      None ,
+      None,
+      List(),
+      List(),
+      None,
+      None,
+      Singleton)
+
+    val d= Default(Lazy,
+                   5000,
+                   Optional)
+
+    val bpm =BlueprintMetadata(d,List(destBean))
+    val bp =bpp (xml)
+    
+    bp mustNot throwA[Exception]
+    bp mustEqual bpm
+
+
+  }
 }
